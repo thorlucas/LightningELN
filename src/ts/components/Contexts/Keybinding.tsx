4201b35b97f7fd183a47f8998da61a4816bca18e
Thor Correia <thor.lucas@icloud.com>
2021-06-15 04:05:52 -0700

    added keybinding group trigger

--- src/ts/components/Contexts/Keybinding.tsx
+++ src/ts/components/Contexts/Keybinding.tsx
@@ -1,4 +1,5 @@
-import React, { KeyboardEvent, useCallback } from "react";
+import isHotkey from "is-hotkey";
+import React, { KeyboardEvent as ReactKeyboardEvent, useCallback, useState } from "react";
 
 type KeybindingGroupWrapperRenderer = ({ attributes, children }: {
 	attributes: any,
@@ -17,14 +18,26 @@ const defaultWrapperRenderer: KeybindingGroupWrapperRenderer = ({ attributes, ch
 	</div>
 );
 
+type Keybinding = {
+	match: (event: KeyboardEvent) => boolean,
+	callback: () => void,
+}
+
 export const KeybindingGroup: React.FC<KeybindingGroupProps> = ({
 	wrapperAttributes = {},
 	renderWrapper = defaultWrapperRenderer,
 	children,
 }) => {
-	const onKeyDown = useCallback((event: KeyboardEvent) => {
-		console.log("Keyed!");
-	}, []);
+	const [keybindings, setKeybindings] = useState<Keybinding[]>([]);
+
+	const onKeyDown = useCallback((event: ReactKeyboardEvent) => {
+		// FIXME: Terribly non-performant because it will run every check on every
+		// keybinding every time
+		const nativeEvent = event.nativeEvent;
+		keybindings
+			.filter(keybinding => keybinding.match(nativeEvent))
+			.forEach(keybinding => keybinding.callback());
+	}, [keybindings]);
 
 	return renderWrapper({
 		children: children,
